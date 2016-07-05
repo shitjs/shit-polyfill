17da64b77bf6413c373a15a7c5fa6bb5cad12a9e
Denys Dovhan <email@denysdovhan.com>
2016-07-05 13:49:55 +0300

    Initial commit

---
+++ window.js
@@ -0,0 +1,62 @@
+(function (window) {
+  var title = 'Wanna use some java-script? Fuck you, sucker!';
+  window.alert = window.alert.bind(window, title);
+
+  var ignore = [
+    'alert', 'document'
+  ];
+
+  function override(obj) {
+
+    for (var prop in obj) {
+      if (ignore.indexOf(prop) > -1) continue;
+
+      switch (typeof obj[prop]) {
+        case 'object':
+          override(prop);
+          break;
+        case 'string':
+          obj[prop] = title;
+          break;
+        case 'function':
+          obj[prop] = function () {
+            alert(title);
+          }
+          break;
+        case 'number':
+          break;
+      }
+    }
+  }
+
+  override(window);
+
+  window.console = {
+    log: alert,
+    assert: alert,
+    clear: alert,
+    constructor: alert,
+    count: alert,
+    debug: alert,
+    dir: alert,
+    dirxml: alert,
+    error: alert,
+    group: alert,
+    groupCollapsed: alert,
+    groupEnd: alert,
+    info: alert,
+    log: alert,
+    markTimeline: alert,
+    profile: alert,
+    profileEnd: alert,
+    table: alert,
+    time: alert,
+    timeEnd: alert,
+    timeStamp: alert,
+    timeline: alert,
+    timelineEnd: alert,
+    trace: alert,
+    warn: alert,
+  };
+
+})(window);
